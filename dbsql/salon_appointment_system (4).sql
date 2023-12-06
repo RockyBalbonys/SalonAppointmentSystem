aceab0d4efffe0735fd6079a0fd85516b896e403
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2023-12-06 22:03:19 +0800

    updated sql

--- dbsql/salon_appointment_system (4).sql
+++ dbsql/salon_appointment_system (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 03:18 AM
+-- Generation Time: Dec 06, 2023 at 03:02 PM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.0
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `salon_appointment_system`
 --
-CREATE DATABASE IF NOT EXISTS `salon_appointment_system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `salon_appointment_system`;
 
 -- --------------------------------------------------------
 
@@ -39,7 +37,8 @@ CREATE TABLE `tbl_admin_users` (
 -- Dumping data for table `tbl_admin_users`
 --
 
-INSERT INTO `tbl_admin_users` (`admin_id`, `admin_username`, `admin_password`) VALUES(1, 'admin', 'admin');
+INSERT INTO `tbl_admin_users` (`admin_id`, `admin_username`, `admin_password`) VALUES
+(1, 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -50,6 +49,7 @@ INSERT INTO `tbl_admin_users` (`admin_id`, `admin_username`, `admin_password`) V
 CREATE TABLE `tbl_bookings` (
   `booking_id` int NOT NULL,
   `booking_user` int NOT NULL,
+  `booking_service` int NOT NULL,
   `booking_date` date NOT NULL,
   `booking_time` time(6) NOT NULL,
   `booking_status` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1'
@@ -59,8 +59,39 @@ CREATE TABLE `tbl_bookings` (
 -- Dumping data for table `tbl_bookings`
 --
 
-INSERT INTO `tbl_bookings` (`booking_id`, `booking_user`, `booking_date`, `booking_time`, `booking_status`) VALUES(19, 1, '0000-00-00', '00:00:00.000000', '1');
-INSERT INTO `tbl_bookings` (`booking_id`, `booking_user`, `booking_date`, `booking_time`, `booking_status`) VALUES(20, 1, '2023-12-21', '21:40:00.000000', '1');
+INSERT INTO `tbl_bookings` (`booking_id`, `booking_user`, `booking_service`, `booking_date`, `booking_time`, `booking_status`) VALUES
+(19, 1, 3, '0000-00-00', '00:00:00.000000', '1'),
+(20, 1, 1, '2023-12-21', '21:40:00.000000', '1'),
+(21, 3, 0, '2023-12-14', '17:33:00.000000', '1'),
+(22, 3, 0, '2023-12-14', '17:33:00.000000', '1'),
+(23, 3, 0, '2023-12-14', '17:33:00.000000', '1'),
+(24, 3, 4, '2023-12-16', '20:00:00.000000', '1'),
+(25, 6, 1, '2023-12-29', '13:56:00.000000', '1'),
+(26, 1, 1, '2023-12-31', '12:45:00.000000', '1'),
+(27, 1, 1, '2023-12-31', '12:45:00.000000', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_booking_services`
+--
+
+CREATE TABLE `tbl_booking_services` (
+  `service_id` int NOT NULL,
+  `service` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `service_cost` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_booking_services`
+--
+
+INSERT INTO `tbl_booking_services` (`service_id`, `service`, `service_cost`) VALUES
+(1, 'Haircut', 1100),
+(2, 'Hair Rebond', 1200),
+(3, 'Hair Color', 1000),
+(4, 'Hair Brazilian', 2000),
+(5, 'Hair Highlights', 1400);
 
 -- --------------------------------------------------------
 
@@ -77,9 +108,10 @@ CREATE TABLE `tbl_booking_status` (
 -- Dumping data for table `tbl_booking_status`
 --
 
-INSERT INTO `tbl_booking_status` (`booking_status_id`, `booking_status`) VALUES(1, 'upcoming');
-INSERT INTO `tbl_booking_status` (`booking_status_id`, `booking_status`) VALUES(2, 'done');
-INSERT INTO `tbl_booking_status` (`booking_status_id`, `booking_status`) VALUES(3, 'deleted');
+INSERT INTO `tbl_booking_status` (`booking_status_id`, `booking_status`) VALUES
+(1, 'upcoming'),
+(2, 'done'),
+(3, 'deleted');
 
 -- --------------------------------------------------------
 
@@ -96,9 +128,10 @@ CREATE TABLE `tbl_gender` (
 -- Dumping data for table `tbl_gender`
 --
 
-INSERT INTO `tbl_gender` (`gender_id`, `gender`) VALUES(1, 'male');
-INSERT INTO `tbl_gender` (`gender_id`, `gender`) VALUES(2, 'female');
-INSERT INTO `tbl_gender` (`gender_id`, `gender`) VALUES(3, 'others');
+INSERT INTO `tbl_gender` (`gender_id`, `gender`) VALUES
+(1, 'male'),
+(2, 'female'),
+(3, 'others');
 
 -- --------------------------------------------------------
 
@@ -120,11 +153,13 @@ CREATE TABLE `tbl_users` (
 -- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_gender`, `user_phonenumber`) VALUES(1, 'rockybalbonys@gmail.com', 'wahaha', 'Rocky', 'Balbonys', '1', '09999999999');
-INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_gender`, `user_phonenumber`) VALUES(2, 'thomason@gmail.com', 'wahaha1', 'Thomason', 'McKinley', '1', '09123456789');
-INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_gender`, `user_phonenumber`) VALUES(3, 'analyn@gmail.com', 'wahaha2', 'Analyn', 'Muko', '3', '09192913123');
-INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_gender`, `user_phonenumber`) VALUES(4, 'larrybato@gmail.com', 'wahaha123', 'Larry', ' bato', '1', ' 0917356284');
-INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_gender`, `user_phonenumber`) VALUES(5, 'harrypota@yahoo.com', 'wahaha5', 'Harry', ' Potter', '3', ' 0937548975');
+INSERT INTO `tbl_users` (`user_id`, `user_email`, `user_password`, `user_firstname`, `user_lastname`, `user_gender`, `user_phonenumber`) VALUES
+(1, 'rockybalbonys@gmail.com', 'wahaha', 'Rocky', 'Balbonys', '1', '09999999999'),
+(2, 'thomason@gmail.com', 'wahaha1', 'Thomason', 'McKinley', '1', '09123456789'),
+(3, 'analyn@gmail.com', 'wahaha2', 'Analyn', 'Muko', '3', '09192913123'),
+(4, 'larrybato@gmail.com', 'wahaha123', 'Larry', ' bato', '1', ' 0917356284'),
+(5, 'harrypota@yahoo.com', 'wahaha5', 'Harry', ' Potter', '3', ' 0937548975'),
+(6, 'prince@yahoo.com', 'wahaha', 'prince', ' lawrence', '', ' 0917356284');
 
 --
 -- Indexes for dumped tables
@@ -141,6 +176,12 @@ ALTER TABLE `tbl_admin_users`
 --
 ALTER TABLE `tbl_bookings`
   ADD PRIMARY KEY (`booking_id`);
+
+--
+-- Indexes for table `tbl_booking_services`
+--
+ALTER TABLE `tbl_booking_services`
+  ADD PRIMARY KEY (`service_id`);
 
 --
 -- Indexes for table `tbl_booking_status`
@@ -174,7 +215,13 @@ ALTER TABLE `tbl_admin_users`
 -- AUTO_INCREMENT for table `tbl_bookings`
 --
 ALTER TABLE `tbl_bookings`
-  MODIFY `booking_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `booking_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `tbl_booking_services`
+--
+ALTER TABLE `tbl_booking_services`
+  MODIFY `service_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_booking_status`
@@ -192,7 +239,7 @@ ALTER TABLE `tbl_gender`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
