036d51949c7db8544d1167d899078fadc55477c5
julsbbb <bautista.juliet.bscs2021@gmail.com>
2023-12-07 09:46:51 +0800

    updated userprofile and clienthomepage

--- client/clienthomepage.php
+++ client/clienthomepage.php
@@ -10,22 +10,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="">
+    <link rel="icon" type="img/png" href="#">
+
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://kit.fontawesome.com/884b91a3a4.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="script.js" defer></script>
+
+
+    <style>
+        /* Custom styles */
+        body {
+            background-color: #fcf0e1;
+        }
+        .navbar {
+            background-color: #b55e5a;
+            color: #fff;
+        }
+        .container {
+            margin-top: 20px;
+        }
+        .picture-box {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            margin-top: 20px;
+        }
+        .picture-item {
+            margin: 10px;
+            width: 200px;
+            cursor: pointer;
+        }
+        .picture-item img {
+            width: 100%;
+            height: auto;
+            object-fit: cover;
+            border-radius: 5px;
+            transition: border 0.3s ease-in-out;
+        }
+        .picture-item p {
+            margin-top: 5px;
+            font-size: 14px;
+        }
+        .picture-item.selected img {
+            border: 2px solid #b55e5a; /* Change border color to indicate selection */
+        }
+  
+
+    </style>
+
+           
+
 </head>
 <body>
+<navbar class="navbar">
 
-       
-        <div class="container container-sm align-items-center justify">
-        <navbar class="navbar border">
-            <div><a href="landingpage.php">Salon Name</a></div>
+            <div><a href="landingpage.php">recover.hair</a></div>
             <div><a href="user_profile.php"><?php echo $_SESSION["user_firstname"]; ?></a><span> | </span><a href="logout.php">logout</a></div>
         </navbar>
-            <label>Services</label>
+        <div class="container">
+        
+<br>
+<br>
+<br>
+
+        <label><h1>Services</h1></label>
+
+            <div class= "picture-box">
             <form method = "get" class="form-group text-center">
                 <div class="card-container row">
                     <div class="card col-3 ">
                         <label for="">
-                            <img src="assets/haircut.jpg" alt=""  class="h-100 w-50">
+                            <img src="assets/haircut.jpg" alt="" class="h-100 w-50">
+                            <br>
                             Haircut
                             (P1100)
                         </label>
@@ -34,6 +94,7 @@
                     <div class="card col-3">
                         <label for="">
                             <img src="assets/rebond.jpg" alt="" class="h-100 w-50">
+                            <br>
                             Hair Rebond
                             (P1200)
                         </label>
@@ -42,6 +103,7 @@
                     <div class="card col-3">
                         <label for="">
                             <img src="assets/color.jpg" alt="" class="h-100 w-50">
+                            <br>
                             Hair Color
                             (P1000)
                         </label>
@@ -50,6 +112,7 @@
                     <div class="card col-3">
                         <label for="">
                             <img src="assets/brazilian.jpg" alt="" class="h-100 w-50">
+                            <br>
                             Hair Brazilian
                             (P2000)
                         </label>
@@ -58,18 +121,67 @@
                     <div class="card col-3">
                         <label for="">
                             <img src="assets/highlights.jpg" alt="" class="h-100 w-50">
+                            <br>
                             Hair Highlights
                             (P1400)
                         </label>
                         <br><input type="radio" value=5 name="service">
                     </div>
+                    <div class="card col-3">
+                        <label for="">
+                            <img src="assets/permhair.jpg" alt="" class="h-100 w-50">
+                            <br>
+                            Hair Perm
+                            (P4000)
+                        </label>
+                        <br><input type="radio" value=3 name="service">
+                    </div>
+                    <div class="card col-3">
+                        <label for="">
+                            <img src="assets/extension.webp" alt="" class="h-100 w-50">
+                            <br>
+                            Hair Extension
+                            (P3500)
+                        </label>
+                        <br><input type="radio" value=3 name="service">
+                    </div>
+                    <div class="card col-3">
+                        <label for="">
+                            <img src="assets/blowdry.jpg" alt="" class="h-100 w-50">
+                            <br>
+                            Blow Dry
+                            (P1000)
+                        </label>
+                        <br><input type="radio" value=3 name="service">
+                    </div>
+                    <div class="card col-3">
+                        <label for="">
+                            <img src="assets/keratin.jpg" alt="" class="h-100 w-50">
+                            <br>
+                            Keratin Treatment
+                            (P2500)
+                        </label>
+                        <br><input type="radio" value=3 name="service">
+                    </div>
+                    <div class="card col-3">
+                        <label for="">
+                            <img src="assets/hairstyles.webp" alt="" class="h-100 w-50">
+                            <br>
+                            Hair Styling
+                            (P3500)
+                        </label>
+                        <br><input type="radio" value=3 name="service">
+                    </div>
                 </div>
+                   
+               
+            </div>
                 
         <label for="">Select Time and Date:</label>
                 <input name="book_date" type="date" class ="row col-3 mb-2">
                 <input name="book_time" type="time" class ="row col-3 mb-2">
                 <textarea name="book_comment" id="" cols="15" rows="3" placeholder="Comments..." class="row col-4 mb-2"></textarea>
-                <input type="submit" value="Book" class="row col-3 mb-2" >
+                <input type="submit" value="Book" class="row col-3 mb-2"  >
                 
             </form>
            
@@ -100,8 +212,22 @@
             ?>    
         </div>
             
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     
-       
+    <script>
+        // JavaScript to handle image selection
+        document.addEventListener('DOMContentLoaded', function () {
+            let pictures = document.querySelectorAll('.picture-item');
+
+            pictures.forEach(function (picture) {
+                picture.addEventListener('click', function () {
+                    picture.classList.toggle('selected');
+                });
+            });
+        });
+    </script>
 
 </body>
 </html>
--- client/user_profile.php
+++ client/user_profile.php
@@ -9,14 +9,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="img/png" href="#">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://kit.fontawesome.com/884b91a3a4.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="script.js" defer></script>
+
+    
+
+    <style>
+        /* Custom styles */
+        body {
+            background-color: #fcf0e1;
+        }
+        .navbar {
+            background-color: #b55e5a;
+            color: #fff;
+        }
+        .container {
+            margin-top: 20px;
+        }
+
+
+    </style>
+
 </head>
 <body>
 
-        <div class="container">
-            <nav class="navbar border">
-                <div><a href="landingpage.php">Salon Name</a></div>
-                <div><a href="clienthomepage.php">Book</a><span> | </span><a href="logout.php">logout</a></div>
-            </nav>
+            <navbar class="navbar h-100">
+                <div><a href="landingpage.php">recover.hair</a></div>
+                <div><a href="clienthomepage.php">Services</a><span> | </span><a href="logout.php">Logout</a></div>
+            </navbar>
+            <div class="container">
+        
+        <br>
+
               <div>
                  <?php 
                 $user_id = $_SESSION["user_id"];
@@ -28,7 +58,7 @@
                 echo "<div class='h1'>" . $user_firstname . ' ' . "$user_lastname </div>";
                 echo "<div class='label'> $user_phonenumber </div>";
 
-                 echo $_SESSION["user_firstname"]. "'s upcoming Appointment(s)";
+                 echo $_SESSION["user_firstname"]. "'s Upcoming Appointment(s)";
                  $selectAllAppointments = "SELECT * FROM tbl_bookings 
                  JOIN tbl_users 
                  ON tbl_bookings.booking_user = tbl_users.user_id 
