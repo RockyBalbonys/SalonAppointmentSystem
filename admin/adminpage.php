3cdf3d3324bf9cf1abf6f3ce51249626d053e9b3
Jay Ann Rose Gerente <gerente.jayannrose.bscs2021@gmail.com>
2024-02-09 18:07:13 +0800

    added UI to admin page

--- admin/adminpage.php
+++ admin/adminpage.php
@@ -11,20 +11,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salon Admin</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <style></style>
 </head>
 <body>
     <div class="container">
-            <nav class="navbar">
-                <div class="nav-item">ADMIN PAGE</div>
-                <div class="nav-item"> <a href  ="adminpage.php">Bookings</a> | <a href  ="users.php">Users</a> | <a href  ="messages.php">Messages</a> | <a href  ="history.php">History</a> | <a href="logout.php">Log Out</a></div>
-            </nav>
-            <div class="h2">
-                Welcome, Admin!
+            <nav class="navbar navbar-expand-md navbar-light">
+                <div class="container-lg">
+                    <span class="fw-bold text-dark align-center justify-content-start d-none d-md-inline">
+                    Admin Page
+                </span>
+            <div class="justify-content-end d-none d-sm-inline">
+                <ul class="nav justify-content-end">
+                    <li class="nav-item text-dark">
+                        <a class="nav-link text-dark fw-bold" href="adminpage.php">Bookings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark fw-bold" href="users.php">Users</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark fw-bold" href="messages.php">Messages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark fw-bold" href="history.php">History</a>
+                    </li>
+                    <li class="nav-item ms-2 d-none d-md-inline">
+                        <a class="btn btn-dark" href="logout.php">Log Out</a>
+                    </li>
+                </ul>
             </div>
-            <div class="container row text-center mt-5 mb-5">
-                <div class="text-center col-6">
+                </div>      
+            </nav>
+            <div class="text-center mt-4">
+                <h2>Welcome, Admin!</h2>
+            </div>
+            <div class="container row justify-content-center align-items-center text-center mt-5 mb-5">
+                <div class=" lead text-center col-6">
                     Upcoming Appointments: 
                     <div class="text-center h1"><?php 
                     $countActive = "SELECT COUNT(*) AS row_count 
@@ -50,7 +74,7 @@
                    }
                     ?></div>
                 </div>
-                <div class="text-center col-6">
+                <div class="lead text-center col-6">
                     <div class="text-center "></div>Finished Appointments: 
                     <div class="h1"><?php 
                     $countActive = "SELECT COUNT(*) AS row_count 
@@ -91,8 +115,8 @@
                     $query = mysqli_query($conn, $selectAllAppointments);
                     
                     if (mysqli_query($conn, $selectAllAppointments)) {
-                       echo '<table class="table table-bordered" id="bookingTable">
-                                   <thead>
+                       echo '<table class="table table-lg table-hover table-light table-borderless" id="bookingTable">
+                                   <thead class="text-center mb-3">
                                        <tr>
                                            <th>Booking_ID</th>
                                            <th>User</th>
@@ -107,15 +131,15 @@
    
                            while ($row = mysqli_fetch_array($query)) {
                                echo '<tr data-booking-id="' . $row["booking_id"] . '">';
-                               echo '<td>' . $row["booking_id"] . '</td>';
-                               echo '<td>' . $row["user_firstname"] . " " . $row["user_lastname"] . '</td>';
+                               echo '<td class="text-center">' . $row["booking_id"] . '</td>';
+                               echo '<td class="text-center">' . $row["user_firstname"] . " " . $row["user_lastname"] . '</td>';
                                $formattedDate = date('F j, Y', strtotime($row["booking_date"]));
-                               echo '<td>' .  $formattedDate . '</td>';
+                               echo '<td class="text-center">' .  $formattedDate . '</td>';
                                $formattedTime = date('h:i A', strtotime($row["booking_time"]));
-                               echo '<td>' . $formattedTime . '</td>';
-                               echo '<td>' . $row["service"] . '</td>';
-                               echo '<td>' . $row["booking_status"] . '</td>';
-                               echo '<td>' . "<div class='btn delete-btn btn-primary'>Done</div>" . '</td>';
+                               echo '<td class="text-center">' . $formattedTime . '</td>';
+                               echo '<td class="text-center">' . $row["service"] . '</td>';
+                               echo '<td class="text-center">' . $row["booking_status"] . '</td>';
+                               echo '<td class="text-center">' . "<div class='btn btn-sm px-2 delete-btn btn-outline-dark'>Done</div>" . '</td>';
                                echo '</tr>';
                            }
    
@@ -150,6 +174,8 @@
 });
 
         </script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
 <?php
